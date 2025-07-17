84e99c267887ce84d987fc9250961bf235afdf28
Srihari Kurudi <149060503+SrihariKurudi@users.noreply.github.com>
2025-07-17 14:23:47 +0530

    .

--- scripts/config.js
+++ scripts/config.js
@@ -3,6 +3,6 @@ export const sheetConfig = {
   "भैषज्यम्": "https://docs.google.com/spreadsheets/d/1KplgNJU8_JeRnAY0bP0FR2WAB_q8OGxdEviCkewJNA0/gviz/tq?tqx=out:json&sheet=भैषज्यम्",
   "माहानसम्": "https://docs.google.com/spreadsheets/d/1KplgNJU8_JeRnAY0bP0FR2WAB_q8OGxdEviCkewJNA0/gviz/tq?tqx=out:json&sheet=माहानसम्",
   "सङ्गीतादिः": "https://docs.google.com/spreadsheets/d/1KplgNJU8_JeRnAY0bP0FR2WAB_q8OGxdEviCkewJNA0/gviz/tq?tqx=out:json&sheet=सङ्गीतादिः",
-  "मृगपक्षिणः": "https://docs.google.com/spreadsheets/d/1KplgNJU8_JeRnAY0bP0FR2WAB_q8OGxdEviCkewJNA0/gviz/tq?tqx=out:json&sheet=मृगपक्षिणः",
+  "पर्यावरणम्": "https://docs.google.com/spreadsheets/d/1KplgNJU8_JeRnAY0bP0FR2WAB_q8OGxdEviCkewJNA0/gviz/tq?tqx=out:json&sheet=पर्यावरणम्",
   "गणितम्":    "https://docs.google.com/spreadsheets/d/1KplgNJU8_JeRnAY0bP0FR2WAB_q8OGxdEviCkewJNA0/gviz/tq?tqx=out:json&sheet=गणितम्"
 };
